76f0906a064de2e36af58de24a4fb343209eb269
Carlos E <cromo_lp@hotmail.com>
2023-06-25 15:13:03 -0600

    Agregamos carpeta JS en el remoto

---
+++ js/domino.js
@@ -0,0 +1,9 @@
+function checkDominos() {
+    const topNumber = parseInt(document.querySelector('.top input').value);
+    const bottomNumber = parseInt(document.querySelector('.bottom input').value);
+  
+    if (topNumber === 4 && bottomNumber === 6) {
+      alert('¡Felicidades! Has elegido la ficha de dominó "4/6".');
+    }
+  }
+  
