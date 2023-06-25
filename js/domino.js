77b39fc5b2a82aff1727e1cf41aab6e592933419
Carlos E <cromo_lp@hotmail.com>
2023-06-25 16:26:03 -0600

    Agregamos las secciones de ganador y perdedor

--- js/domino.js
+++ js/domino.js
@@ -4,6 +4,8 @@ function checkDominos() {
   
     if (topNumber === 4 && bottomNumber === 6) {
       alert('¡Felicidades! Has elegido la ficha de dominó "4/6".');
+    } else {
+        alert('Respuesta incorrecta')
     }
   }
   
