c637ff9fc96d7d2839725320d36f623ac4a31554
Carlos E <cromo_lp@hotmail.com>
2023-06-25 17:50:17 -0600

    Completamos la funcionalidad del ejercicio con JS

--- js/domino.js
+++ js/domino.js
@@ -1,14 +1,16 @@
 const sectionWin = document.getElementById('win')
 const sectionLose = document.getElementById('lose')
-const topNumber = parseInt(document.querySelector('.top input').value);
-const bottomNumber = parseInt(document.querySelector('.bottom input').value);
-//const btnReload = document.getElementById('reboot')
 
 function checkDominos() {    
+    const topNumber = parseInt(document.querySelector('.top input').value);
+    const bottomNumber = parseInt(document.querySelector('.bottom input').value);
+    const sectionDomino = document.getElementById('ocultar-domino')
     if (topNumber === 4 && bottomNumber === 6) {
         sectionWin.style.display = 'block'
+        sectionDomino.style.display = 'none'
     } else {
         sectionLose.style.display = 'block'
+        sectionDomino.style.display = 'none'
     }
 }
 
