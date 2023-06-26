a50414215502879e81318ed152e2910f2b6ee449
Carlos E <cromo_lp@hotmail.com>
2023-06-25 19:59:50 -0600

    Añadimos estilos CSS

--- js/domino.js
+++ js/domino.js
@@ -5,10 +5,12 @@ function checkDominos() {
     const topNumber = parseInt(document.querySelector('.top input').value);
     const bottomNumber = parseInt(document.querySelector('.bottom input').value);
     const sectionDomino = document.getElementById('ocultar-domino')
+    
     if (topNumber === 4 && bottomNumber === 6) {
         sectionWin.style.display = 'block'
         sectionDomino.style.display = 'none'
-    } else {
+    } 
+     else {
         sectionLose.style.display = 'block'
         sectionDomino.style.display = 'none'
     }
